d29c3f4ae982b7830e3e929519ed86bc91abce20
Marina <gma18@list.ru>
2022-02-11 12:32:25 +0300

    fix: fix getNewWord

--- src/utils/getNewWord.ts
+++ src/utils/getNewWord.ts
@@ -1,6 +1,6 @@
 import { api } from "./api";
 
-export const getNewWord = (value: string) => {
+export const getNewWord_ = (value: string) => {
   return new Promise<{ transcription: string; translation: string }>(
     (resolve, rej) => {
       api.getWordInformation(value).then((res) => {
@@ -15,4 +15,18 @@ export const getNewWord = (value: string) => {
       });
     }
   );
+};
+
+export const getNewWord = async (value: string) => {
+  const res = await api.getWordInformation(value);
+
+  if (res.def[0]) {
+    const wordInfo = res.def[0];
+    const transcription = wordInfo.ts;
+    const translation = wordInfo.tr[0].text;
+
+    return { transcription, translation };
+  }
+
+  throw new Error("Word doesn't exist");
 };
